0a5e7c380ab0c6d965a039aa14b28956b51971f3
Satyam Kumar <satyamkumar629301@gmail.com>
2026-04-12 22:12:23 +0530

    change HoldingSchema

--- backend/schemas/HoldingSchemas.js
+++ backend/schemas/HoldingSchemas.js
@@ -1,12 +1,24 @@
-const {Schema} = require('mongoose');
+const { Schema } = require('mongoose');
 
 const HoldingSchemas = new Schema({
-    name: String,
-    qty: Number,
-    avg: Number,
-    price: Number,
-    net: String,
-    day: String,
+    name: {
+        type: String,
+    },
+    qty: {
+        type: Number,
+    },
+    avg: {
+        type: Number,
+    },
+    price: {
+        type: Number,
+    },
+    net: {
+        type: String,
+    },
+    day: {
+        type: String,
+    },
 });
 
 module.exports = { HoldingSchemas };
