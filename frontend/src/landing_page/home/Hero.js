db863ea06e3d50e388c410315e347a3cade8eacf
Satyam Kumar <satyamkumar629301@gmail.com>
2026-04-14 12:47:54 +0530

    fix button link

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
  
 function Hero() {
-
-    const navigation = useNavigate();
 
     return ( 
         <div className='container p-5 mt-4'>
@@ -12,9 +10,9 @@ function Hero() {
                 <div className='m-auto mt-5'>
                     <h1>Invest in everything</h1>
                     <p>Online platform to invest in stocks, derivatives, mutual funds, and more</p>
-                    <button onClick={() => navigation("/signup")} className='btn btn-primary btn-lg px-5 my-3'>
+                    <Link to="/signup" className='btn btn-primary btn-lg px-5 my-3'>
                         Signup now for free
-                    </button>
+                    </Link>
                 </div>
             </div>
         </div>
